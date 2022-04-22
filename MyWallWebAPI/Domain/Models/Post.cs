c9666305d032ca97c1d0edc7d7f140a2b1f2eb41
Vitor Alves <vitornobody@gmail.com>
2022-04-22 15:39:37 -0300

    User Post relationship - OneToMany

--- MyWallWebAPI/Domain/Models/Post.cs
+++ MyWallWebAPI/Domain/Models/Post.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyWallWebAPI.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,8 @@ namespace MyWallWebAPI
         public string Titulo { get; set; }
         public string Conteudo { get; set; }
         public DateTime Data { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
--- MyWallWebAPI/Domain/Services/PostService.cs
+++ MyWallWebAPI/Domain/Services/PostService.cs
@@ -1,4 +1,5 @@
-﻿using MyWallWebAPI.Infrastructure.Data.Repositories;
+﻿using MyWallWebAPI.Domain.Models;
+using MyWallWebAPI.Infrastructure.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,13 @@ namespace MyWallWebAPI.Domain.Services
     public class PostService
     {
         private readonly PostRepository _postRepository;
+        private readonly AuthService _authService;
 
-        public PostService(PostRepository postRepository)
+        public PostService(PostRepository postRepository, AuthService authService)
         {
+            _authService = authService;
             _postRepository = postRepository;
+            _authService = authService;
         }
 
         public async Task<List<Post>> ListPosts()
@@ -22,9 +26,18 @@ namespace MyWallWebAPI.Domain.Services
             return list;
         }
 
+        public async Task<List<Post>> ListMeusPosts()
+        {
+            ApplicationUser currentUser = await _authService.GetCurrentUser();
+
+            List<Post> list = await _postRepository.ListPostsByApplicationUserId(currentUser.Id);
+
+            return list;
+        }
+
         public async Task<Post> GetPost(int postId)
         {
-            Post post = await _postRepository.GetPost(postId);
+            Post post = await _postRepository.GetPostById(postId);
 
             if (post == null)
                 throw new ArgumentException("Post não existe!");
@@ -32,27 +45,51 @@ namespace MyWallWebAPI.Domain.Services
             return post;
         }
 
-        public async Task<Post> CreatePost(Post post)
+        public async Task<Post> NovoPost(Post post)
         {
-            post.Data = DateTime.Now;
+            ApplicationUser currentUser = await _authService.GetCurrentUser();
 
-            post = await _postRepository.CreatePost(post);
+            Post novoPost = new Post();
 
-            return post;
+            novoPost.ApplicationUserId = currentUser.Id;
+            novoPost.Data = DateTime.Now;
+            novoPost.Titulo = post.Titulo;
+            novoPost.Conteudo = post.Conteudo;
+
+            novoPost = await _postRepository.CreatePost(novoPost);
+
+            return novoPost;
         }
 
         public async Task<int> UpdatePost(Post post)
         {
-            return await _postRepository.UpdatePost(post);
-        }
+            ApplicationUser currentUser = await _authService.GetCurrentUser();
 
-        public async Task<bool> DeletePost(int postId)
-        {
-            Post fndPost = await _postRepository.GetPost(postId);
-            if (fndPost == null)
+            Post findPost = await _postRepository.GetPostById(post.Id);
+            if (findPost == null)
                 throw new ArgumentException("Post não existe!");
 
-            await _postRepository.DeletePost(postId);
+            if (!findPost.ApplicationUserId.Equals(currentUser.Id))
+                throw new ArgumentException("Sem permissão.");
+
+            findPost.Titulo = post.Titulo;
+            findPost.Conteudo = post.Conteudo;
+
+            return await _postRepository.UpdatePost(findPost);
+        }
+
+        public async Task<bool> DeletePostAsync(int postId)
+        {
+            ApplicationUser currentUser = await _authService.GetCurrentUser();
+
+            Post findPost = await _postRepository.GetPostById(postId);
+            if (findPost == null)
+                throw new ArgumentException("Post não existe!");
+
+            if (!findPost.ApplicationUserId.Equals(currentUser.Id))
+                throw new ArgumentException("Sem permissão.");
+
+            await _postRepository.DeletePostAsync(postId);
 
             return true;
         }
